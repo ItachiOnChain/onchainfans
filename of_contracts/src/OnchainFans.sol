e54db3edfbdd2483f6a425128dd4464af7b60e3e
itachionchain <itachionchain@gmail.com>
2025-07-10 18:53:19 +0530

    contract completed

--- of_contracts/src/OnchainFans.sol
+++ of_contracts/src/OnchainFans.sol
@@ -2,7 +2,25 @@
 pragma solidity 0.8.25;
 
 contract OnchainFans {
-    
-    
-    
+    uint constant public FEE = 0.0001 ether;
+
+    mapping (address => mapping (address => bool)) public members;
+
+    error InsufficientFee();
+    error AlreadyAMember();
+    error PaymentRejected();
+    function join(address payable creator) external payable {
+        if(msg.value < FEE) {
+            revert InsufficientFee();
+        }
+        
+        if(members[creator][msg.sender] == true) {
+            revert AlreadyAMember();
+        }
+
+        (bool sent, bytes memory _data) = creator.call{value: FEE}("");
+        if(!sent) {
+            revert PaymentRejected();
+        }
+    }
 }
